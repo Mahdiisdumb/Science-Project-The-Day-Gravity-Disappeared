cad122380decda02bf0cc34a3aa49edec93d97d0
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-05-18 19:44:14 -0400

    gdf

--- Assets/Scripts/sceneman.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class sceneman : MonoBehaviour
-{
-    public void changeScene(int sceneIndex)
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
-    }
-}
--- Assets/Scripts/scs support/world.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-public class WorldRef : MonoBehaviour
-{
-    public string id;
-}
